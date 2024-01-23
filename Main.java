c032eefbe517dfe02c9699ed32bc236a9b6124bb
Don <donmo99@gmail.com>
2024-01-22 18:50:26 -0800

    Second Commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException{
+		//PrintStream output = new PrintStream("bruh.txt");
+		PrintStream output = new PrintStream("C:\\Users\\donmo\\Downloads\\test.txt");
+		output.println("Does this work?"); // IT WORKSSSSSSSSSSSSSSS
+		output.println("rewrite sadly");
+		
+	}
+
+}
